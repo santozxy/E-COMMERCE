91bf925a432f5ca47df394f6e24d3c2b14c37ee9
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-08-14 00:31:41 -0300

    Update

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function proxImg() {
   }
   document.getElementById("radio" + cont).checked = true;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const colorsButton = document.querySelector('.colors');
+    const icon = colorsButton.querySelector('i');
+    let isLightMode = false;
+
+    colorsButton.addEventListener('click', function() {
+        isLightMode = !isLightMode;
+        const newBgColor = isLightMode ? '#f2f2f2' : '#131313';
+        const newTertiaryColor = isLightMode ? '#000' : '#fff';
+        const newIconClass = isLightMode ? 'fa-moon' : 'fa-sun';
+
+        document.documentElement.style.setProperty('--bg', newBgColor);
+        document.documentElement.style.setProperty('--terciary', newTertiaryColor);
+        icon.classList.remove(isLightMode ? 'fa-sun' : 'fa-moon');
+        icon.classList.add(newIconClass);
+    });
+});
